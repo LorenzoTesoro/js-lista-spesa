a1dfc5604ceb72662be4b8250432e3a81004ffe9
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-13 15:02:37 +0200

    add dom manipulation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,19 @@
 
 // 1. Creo un array in cui inserisco le singole istruzioni
 
-const list = [
-    'Comprare il pane',
-    'Comprare il latte',
-    'Comprare la verdure',
-    'Comprare gli snacks',
-    'Comprare il vino'
-]
+const list = ['Comprare il pane', 'Comprare il latte', 'Comprare la verdura','Comprare gli snacks','Comprare il vino']
 
-// 2. Scorrere dentro l'array - ciclo while
+// 2. Stampare a schermo la lista della spesa:
+        // 2.1 - Selezionare l'elemento della dom in cui stampare i valori
+        // 2.2 - creo l'elemento da stampare
+        // 2.3 - stampo i singoli valori nell'elemtento selezionato (innerhtml)
 
+const ulElement = document.querySelector('ul');
 let i = 0;
 
 while(i < list.length){
     const item = list[i];
-    console.log(item);
-
+    ulElement.innerHTML += `<li>${item}<li>`; 
+    //console.log(item);
     i++;
 }
