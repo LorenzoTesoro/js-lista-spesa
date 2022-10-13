9be5f0781eec8a8213deaf6dbed46d5c4ba3adbc
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-10-13 14:34:33 +0200

    add console.log solution

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 /* Consegna: stampare a schermo lista della spesa con ciclo while */
+
+
+// 1. Creo un array in cui inserisco le singole istruzioni
+
+const list = [
+    'Comprare il pane',
+    'Comprare il latte',
+    'Comprare la verdure',
+    'Comprare gli snacks',
+    'Comprare il vino'
+]
+
+// 2. Scorrere dentro l'array - ciclo while
+
+let i = 0;
+
+while(i < list.length){
+    const item = list[i];
+    console.log(item);
+
+    i++;
+}
